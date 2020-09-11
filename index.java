b9a38203ba9424a14797514ff5d1b437ed030977
lukebunn1 <superpantsftw1@gmail.com>
2020-09-11 15:26:43 +0000

    I finished the tank I struggled to make the cannon part longer

--- index.java
+++ index.java
@@ -13,13 +13,13 @@ void draw() {
  
   //this rectangle is the start of your army tank
   //all of your programming should be after this 👍 
-  rect(200, 200, 100, 50);
-  
-  
- 
-
-  
- 
-	
-
+  rect(201, 200, 100, 50);
+  ellipse(150, 225, 20, 20);
+  ellipse(170, 225, 20, 20);
+  ellipse(190, 225, 20, 20);
+  ellipse(210, 225, 20, 20);
+  ellipse(230, 225, 20, 20);
+  ellipse(250, 225, 20, 20);
+	rect(175, 163, 50, 25)
+  rect(225, 155, 50, 10)
 }
